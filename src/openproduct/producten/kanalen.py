5d21398eb30ba8c4b47831a9189461eed6dce605
Floris272 <floris@maykinmedia.nl>
2025-04-04 15:12:23 +0200

    :memo: [#9] fix api spec custom settings

--- src/openproduct/producten/kanalen.py
+++ src/openproduct/producten/kanalen.py
@@ -10,4 +10,5 @@ KANAAL_PRODUCTEN = Kanaal(
         "producttype.uniforme_product_naam",
         "producttype.code",
     ),
+    extra_kwargs={"producttype.id": {"help_text": "uuid van het producttype"}},
 )
