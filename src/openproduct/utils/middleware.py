8311e52cfee8d5e84b537910a9ed6e25928853e0
Floris272 <floris@maykinmedia.nl>
2025-04-11 17:24:58 +0200

    :bug: [#104] use localemiddleware for api only

---
+++ src/openproduct/utils/middleware.py
@@ -0,0 +1,8 @@
+from django.middleware.locale import LocaleMiddleware as _LocaleMiddleware
+
+
+class APILocaleMiddleware(_LocaleMiddleware):
+
+    def process_request(self, request):
+        if "api" in request.path:
+            super().process_request(request)
