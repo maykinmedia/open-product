13d89f33e47b6834d906e6b549caf279e8b2b812
Floris272 <floris@maykinmedia.nl>
2025-05-19 14:54:39 +0200

    :globe_with_meridians: [#137] add pagination translation

--- src/openproduct/utils/pagination.py
+++ src/openproduct/utils/pagination.py
@@ -1,7 +1,18 @@
+from rest_framework.response import Response
 from rest_framework.pagination import PageNumberPagination
-
+from django.utils.translation import gettext_lazy as _
 
 class Pagination(PageNumberPagination):
     page_size = 100
     page_size_query_param = "page_size"
     max_page_size = 200
+
+    def get_paginated_response(self, data):
+        return Response(
+            {
+                _("aantal"): self.page.paginator.count,
+                _("volgende"): self.get_next_link(),
+                _("vorige"): self.get_previous_link(),
+                _("resultaten"): data,
+            }
+        )
