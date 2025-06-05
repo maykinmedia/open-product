b9cfed4cb6b2b3103b59cbfd460b0e3727aa4be7
Floris272 <floris@maykinmedia.nl>
2025-06-05 14:19:02 +0200

    :bug: fix duplicate name issue in load upl command

--- src/openproduct/producttypen/tests/test_load_upl.py
+++ src/openproduct/producttypen/tests/test_load_upl.py
@@ -128,7 +128,8 @@ class TestLoadUPLCommand(TestCase):
 
     def test_upn_is_updated(self):
         UniformeProductNaamFactory.create(
-            uri="http://standaarden.overheid.nl/owms/terms/AangifteVertrekBuitenland"
+            uri="http://standaarden.overheid.nl/owms/terms/AangifteVertrekBuitenland",
+            naam="aangifte vertrek buitenland"
         )
 
         result = self.call_command("--file", self.path)
