3d500b92e21ef853f65791c00bc306007de8e0c1
Floris272 <floris@maykinmedia.nl>
2025-08-26 15:23:32 +0200

    :sparkles: [#187] Add urn app

---
+++ src/openproduct/urn/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+from openproduct.urn.fields import BaseUrnField
+
+
+class UrnMappingConfig(models.Model):
+    urn = BaseUrnField(verbose_name=_("Urn"), help_text=_("Urn"), unique=True)
+
+    url = models.URLField(
+        verbose_name=_("basis url"),
+        help_text=_("Basis url van een urn."),
+        unique=True,
+    )
+
+    class Meta:
+        verbose_name = _("Urn Mapping")
+        verbose_name_plural = _("Urn Mappings")
+        unique_together = ("urn", "url")
