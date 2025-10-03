060df5e99a2401de9317bdc068cd2d19d43dea46
Floris272 <floris@maykinmedia.nl>
2025-10-03 18:02:32 +0200

    :white_check_mark: [#224] fix test

--- src/openproduct/producttypen/tests/api/test_thema.py
+++ src/openproduct/producttypen/tests/api/test_thema.py
@@ -524,7 +524,7 @@ class TestThemaViewSet(BaseApiTestCase):
         thema_b = ThemaFactory.create(hoofd_thema=thema_a)
         thema_c = ThemaFactory.create(hoofd_thema=thema_b)
 
-        data = {"hoofd_thema", thema_c.uuid}
+        data = {"hoofd_thema": thema_c.uuid}
         response = self.client.patch(self.detail_path(thema_a), data)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertEqual(
