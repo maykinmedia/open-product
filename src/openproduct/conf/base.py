da054c287976fe22f4e593ae28344255d747e357
Daniel Mursa <daniel@maykinmedia.nl>
2026-09-02 09:56:05 +0200

    :arrow_up: [maykinmedia/open-api-framework#223] Include templates from maykin-common 0.20.1

--- src/openproduct/conf/base.py
+++ src/openproduct/conf/base.py
@@ -5,7 +5,7 @@ from django.utils.translation import gettext_lazy as _
 from celery.schedules import crontab
 
 os.environ["_USE_STRUCTLOG"] = "True"
-
+from maykin_common.branding import ProductDefinition
 from maykin_common.config import DocumentationParams, config
 from open_api_framework.conf.base import *  # noqa: F403
 
@@ -137,6 +137,66 @@ ADMIN_INDEX_DISPLAY_DROP_DOWN_MENU_CONDITION_FUNCTION = (
 )
 
 ADMIN_INDEX_SHOW_REMAINING_APPS = False
+
+#
+# MAYKIN-COMMON branding
+#
+MKN_BRANDING_PRODUCT_DEFINITION = ProductDefinition(
+    name="Open Product",
+    hyperlink="https://github.com/maykinmedia/open-product",
+    logo_path="ico/open-product-icon.svg",
+)
+
+custom_product_name: str = config(
+    "CUSTOM_PRODUCT_NAME",
+    default="",
+    documentation=DocumentationParams(
+        help_text=(
+            "Specify the custom product name when redistributing the application, e.g. "
+            "as part of your own software suite."
+        ),
+        group="Branding",
+    ),
+)
+custom_product_url: str = config(
+    "CUSTOM_PRODUCT_URL",
+    default="",
+    documentation=DocumentationParams(
+        help_text=(
+            "Optional link for the custom product when redistributing the "
+            "application. If provided, the product name will be clickable."
+        ),
+        group="Branding",
+    ),
+)
+custom_product_logo_path: str = config(
+    "CUSTOM_PRODUCT_LOGO_PATH",
+    default="",
+    documentation=DocumentationParams(group="Branding"),
+)
+custom_product_logo_url: str = config(
+    "CUSTOM_PRODUCT_LOGO_URL",
+    default="",
+    documentation=DocumentationParams(
+        help_text=(
+            "Optional link for the custom product logo when redistributing the "
+            "application. When using externally hosted assets, note that you may "
+            "need to tweak the Content-Security-Policy settings."
+        ),
+        group="Branding",
+    ),
+)
+MKN_BRANDING_DERIVED_PRODUCT_DEFINITION = (
+    ProductDefinition(
+        name=custom_product_name,
+        hyperlink=custom_product_url,
+        logo_path=custom_product_logo_path,
+        logo_url=custom_product_logo_url,
+    )
+    if custom_product_name
+    else None
+)
+
 
 #
 # reversion_compare
